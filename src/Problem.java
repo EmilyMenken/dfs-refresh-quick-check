a595edf615e9dfa8353c2db1e7874f168432d9ff
EmilyM <155667566+EmilyMenken@user.noreply.github.com>
2025-05-27 11:22:07 -0700

    finished problem.java, test passing

--- src/Problem.java
+++ src/Problem.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Problem {
     /**
      * Returns how many salamanders less than 3 years old will be at risk from a
@@ -8,13 +10,50 @@ public class Problem {
      * and so on. Salamanders of any age can catch and spread the disease, but
      * it is only a serious risk for salamanders strictly less than 3 years old.
      * 
-     * Include the initial patient in the count iff the initial patient is less
+     * Include the initial patient in the count if the initial patient is less
      * than 3 years old.
      * 
      * @param initialPatient the first salamander to be infected
      * @return the number of salamanders less than 3 years old that may be infected
      */
     public static int countSeriousInfections(Salamander initialPatient) {
-        return -1;
-    }
-}
+        
+        //getAge < 3
+        
+        Set<Salamander> visited = new HashSet<>();
+        List<Salamander> infected = new ArrayList<>();
+        
+        countSeriousInfectionsHelper(initialPatient, visited, infected);
+        
+        return infected.size();
+
+    }//end countSeriousInfections
+
+    public static void countSeriousInfectionsHelper(Salamander initialPatient, Set<Salamander> visited, List<Salamander> infected){
+
+
+        if(initialPatient == null || visited.contains(initialPatient)){
+
+            return;
+
+        }//end if
+
+         visited.add(initialPatient);
+
+        if(initialPatient.getAge() <3){
+
+            infected.add(initialPatient);
+
+        }//end if
+
+        for(Salamander s : initialPatient.getContacts()){
+
+            countSeriousInfectionsHelper(s, visited, infected);
+
+        }//end for
+
+    }//end countSeriousInfectionsHelper
+
+
+
+}//end file
